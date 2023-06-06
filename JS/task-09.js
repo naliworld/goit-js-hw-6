b2946c61509226930f5fd701413f0ded9363e31c
naliworld <123972576+naliworld@users.noreply.github.com>
2023-06-06 13:37:34 +0200

    Update task-09.js

--- JS/task-09.js
+++ JS/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const colorDescriptionRef = document.querySelector(".color");
+const colorChangeButtonRef = document.querySelector(".change-color");
+const bodyRef = document.querySelector("body");
+
+colorChangeButtonRef.addEventListener("click", () => {
+  colorDescriptionRef.textContent = getRandomHexColor();
+
+  bodyRef.style.backgroundColor = `${colorDescriptionRef.textContent}`;
+});
