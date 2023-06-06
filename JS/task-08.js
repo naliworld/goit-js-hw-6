9e7b008d1679b138326b45c2fbb760346083e1af
naliworld <123972576+naliworld@users.noreply.github.com>
2023-06-06 13:37:36 +0200

    Update task-08.js

--- JS/task-08.js
+++ JS/task-08.js
@@ -1,1 +1,22 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", onGetFormData);
+
+function onGetFormData(event) {
+  event.preventDefault();
+
+  const { email, password } = event.currentTarget.elements;
+  // console.log(event.currentTarget.elements);
+
+  if (email.value.trim() === "" || password.value.trim() === "") {
+    return alert("All fields must be filled!");
+  }
+
+  const formData = {
+    email: email.value,
+    password: password.value,
+  };
+
+  loginForm.reset();
+  console.log(formData);
+}
