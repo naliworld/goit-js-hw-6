a900dbe907750b5178fbf73b41ce2dfa50071f08
naliworld <123972576+naliworld@users.noreply.github.com>
2023-06-06 13:37:48 +0200

    Update task-02.js

--- JS/task-02.js
+++ JS/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsRef = document.querySelector("#ingredients");
+
+const elements = ingredients.map((el) => {
+  const listRef = document.createElement("li");
+  listRef.classList.add("item");
+  listRef.textContent = el;
+
+  return listRef;
+});
+
+ingredientsRef.append(...elements);
