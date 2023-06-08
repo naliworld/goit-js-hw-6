d8c5552c45dd96b480b01d40c7aee907e7931d75
naliworld <123972576+naliworld@users.noreply.github.com>
2023-06-08 13:39:18 +0200

    Update task-01.js

--- JS/task-01.js
+++ JS/task-01.js
@@ -1,6 +1,7 @@
 const listEl = document.querySelectorAll(".item");
 
-console.log(`Number of categories: `, listEl);
+
+console.log(`Number of categories: `, listEl.length);
 
 listEl.forEach((el) =>
   console.log(`Category: `, el.firstElementChild.textContent),
